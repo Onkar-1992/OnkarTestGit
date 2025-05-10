356d3d2eaf9ac511f98c3c8975e4d5c495b33637
Onkar-1992 <onkarthopate11@gmail.com>
2025-05-11 02:38:05 +0530

    Change in Test Runner to add OnkarTestGit folder name

--- OnkarTestGit/src/test/java/StepDefinitions/TestRunner.java
+++ OnkarTestGit/src/test/java/StepDefinitions/TestRunner.java
@@ -38,7 +38,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/test/resources/Features",glue= {"StepDefinitions"},
+		features="OnkarTestGit/src/test/resources/Features",glue= {"StepDefinitions"},
 				tags = "@setup or @registration",
     plugin = {"pretty", "html:target/cucumber-reports"}
     	//	tags = "@setup and @registration"
